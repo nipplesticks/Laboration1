f84beb80bdffaa74d1bbbcd3e1403c748acb3db8
HENRIK NILSSON <heni16@g332-0020>
2017-09-13 14:19:57 +0000

    Mother program with status checking

--- lab2.cpp
+++ lab2.cpp
@@ -2,26 +2,34 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <ctime>
-
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
 	int value = 0;
+	time_t bug = time(0);
 	//pid_t myPid = static_cast<pid_t>(argv[1]);
 	bool canIDie = argc == 2;
 	time_t startTime = time(0);
-
-	while (time(0) - startTime < 20 && !canIDie)
+	int* tal = new int[10];	
+	while (time(0) - startTime < 20)
 	{
 		// std::cout << value++ << std::endl;
 		//std::cout << 0 << std::endl;
 
 		value++;
+		if(canIDie && (time(0) - bug > 10))
+		{
+			int death = 10/0;
+//			tal[99] = -1;
+//			delete[] tal;
+//			std::cout << tal[99]  << std::endl;
+	 	}
 	}
 
 	value = 0;
 	
-	std::cout << "Child with pid: " << getpid() << " died at: " << time(0) << std::endl;	
-
+//	std::cout << "Child with pid: " << getpid() << " died at: " << time(0) << std::endl;	
+	exit(0);
 	return 0;
 }
--- mother.cpp
+++ mother.cpp
@@ -2,61 +2,42 @@
 #include <sys/signal.h>
 #include <iostream>
 #include <vector>
-#include <wait.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
 
 int main()
 {
-    int choice = -1;
-    bool isChild = false;
-    std::vector<pid_t> pids;
+	int choice = -1;
+	bool isChild = false;
+	    std::vector<pid_t> pids;
 
 
-    std::cout << "Would you like to fork()?\n";
-    std::cout << "How many children do you want?";
-    std::cin >> choice;
+	    std::cout << "Would you like to fork()?\n";
+	    std::cout << "How many children do you want?";
+	    std::cin >> choice;
 
+	pids.push_back(fork());
+	if(!pids.back()) execlp("./n_infinity", "./n_infinity","0",  NULL);
 
-    for (int i = 0; i < choice && !isChild; i++)
-    {
-        pids.push_back(fork());
+	    for (int i = 0; i < choice - 1 && !isChild; i++)
+	    {
+		pids.push_back(fork());
+		if (!pids.back())
+		{
+		    execlp("./n_infinity", "./n_infinity", NULL);
+		}
 
-        if (pids.back() == isChild)
-        {
-            isChild = true;
-            execlp("./infinity", "./infinity", NULL);
-        }
+	    }
 
-    }
-
-
-    if (!isChild)
-    {
-	int poo = 0;
-
-        wait(NULL);
-	/*
-	while (1)
+		
+	for(int i = 0; i < pids.size(); i++)
 	{
-		//std::cout << std::to_string(poo++) << std::endl;
-		//std::cout << "Hej" << std::endl;
-		poo++;
-	}
-	*/
+		int status;
+		waitpid(pids[i], &status, 0);
+		std::cout << "PARENT: Child: " << pids[i] << " Exit status: " << (WIFSIGNALED(status) ? "Forced!!!!!": "Normal")  << " ,Signal:  " << status<< std::endl;
+	}  
 
-
-        /*
-        int time = -1;
-        for (int i = 0; i < 100000; i++)
-            time++;
-
-
-        for (int i = 0; i < choice; i++)
-            {
-                kill(pids.back(), 9);
-                pids.pop_back();
-            }
-            */
-    }
-
+	std::cout << "MUTTER IST DEAD" << std::endl;
     return 0;
 }
